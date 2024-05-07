d1e66cea78062ba24d0eb513e2b219a4b2b3c53b
Juan Cruz Marquez <juan.marquez@vemo.global>
2024-05-06 21:12:50 -0300

    Add positions.tsx file with list of positions

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -4,17 +4,14 @@ import axios from 'axios'
 import { Countries } from './countries';
 import { Position } from './types/Positions.type';
 import { Difficulty } from './types/Difficulty.type';
-
-
-
-
+import { positions } from './positions';
 
 export type PlayerCharacteristic = {
   name: string
   apiName: string
   type: 'string' | 'number'
   placeholder: string
-  component?: (props: any) => JSX.Element
+  component?: JSX.Element
 }
 
 const url = 'http://localhost:3000/'
@@ -35,126 +32,38 @@ function App() {
   const [characteristicSelected, setCharacteristicSelected] = useState<PlayerCharacteristic>()
   const [player, setPlayer] = useState<Player>()
   const [dailyPlayers, setDailyPlayers] = useState<DailyPlayers>()
+  const [selectedPositions, setSelectedPositions] = useState<Position[]>([])
+
+
   const debug = false
   const border = 'border border-red-400'
   const orderedCountries = Countries.sort((a, b) => a.name.localeCompare(b.name))
 
-  const positions: Position[] = [
-    Position.CM,
-    Position.CB,
-    Position.ST,
-    Position.CDM,
-    Position.LM,
-    Position.RM,
-    Position.CAM,
-    Position.GK,
-    Position.RB,
-    Position.LB,
-    Position.LW,
-    Position.RW,
-    Position.CF,
-    Position.RWB,
-    Position.LWB,
-  ]
-  
-  const characteristics: PlayerCharacteristic[] = [
-    {
-      name: 'Nationality',
-      apiName: 'nationality',
-      type: 'string',
-      placeholder: 'Select the player nationality',
-      component: (props: any) => (
-        <div className='flex justify-center gap-2'>
-          <select >
-            {orderedCountries.map((country, index) => (
-              <option key={index}>
-                <img src={`https://flagsapi.com/${country.code}/flat/64.png`}/>
-                {country.name}</option>
-            ))}
-          </select>
-        </div>
-      )
-    },
-    {
-      name: 'Height',
-      apiName: 'heightCm',
-      type: 'number',
-      placeholder: 'Enter the height of the player',
-      component: (props: any) => (
-        <div>
-        <input {...props} step="1" type='number' placeholder='Enter the height of the player' max={250} min={110} defaultValue={160} key='height'/>
-        </div>
-        
-      )
-    },
-    {
-      name: 'Weight',
-      apiName: 'weightKgs',
-      type: 'number',
-      placeholder: 'Enter the weight of the player',
-      component: (props: any) => (
-        <div className='flex justify-center gap-2'>
-        <input step="1" type='number' placeholder='Enter the weight of the player' max={200} min={40} defaultValue={70} key='weight'/>
-        <button
-              className={`bg-green-400 text-white p-2 font-bold rounded ${!characteristicSelected ? 'bg-gray-400 cursor-not-allowed' : 'hover:bg-green-500'}`}
-              disabled={!characteristicSelected}
-            >
-              Submit
-        </button>
-        </div>
-      )
-    },
-    {
-      name: 'Preferred Foot',
-      apiName: 'preferredFoot',
-      type: 'string',
-      placeholder: 'Enter the preferred foot of the player',
-      component: (props: any) => (
-        <div className='flex justify-center gap-2'>
-          <button className='rounded bg-green-400 hover:bg-green-500 text-white font-bold py-2 px-6' value='Left'>Left</button>
-          <button className='rounded bg-green-400 hover:bg-green-500 text-white font-bold py-2 px-6' value='Right'>Right</button>
-        </div>
-      )
-    },
-    {
-      name: 'Positions',
-      apiName: 'positions',
-      type: 'string',
-      placeholder: 'Enter the positions of the player',
-      component: (props: any) => (
-        <div className='grid grid-cols-5 gap-2'>
-          {positions.map((position: Position, index: number) => (
-            <button
-              key={index}
-              className={`rounded bg-green-400/60 hover:bg-green-500 text-white font-bold py-2 px-6`}
-              value={position}
-            >
-              {position}
-            </button>
-          ))}
-        </div>
-      )
-    },
-    {
-      name: 'Birth Date',
-      apiName: 'birthDate',
-      type: 'string',
-      placeholder: 'Enter the birth date of the player',
-      component: (props: any) => (
-        <div className='flex justify-center gap-2'>
-          <input  type='date' placeholder='Enter the birth date of the player' key='birthDate' max={new Date().toISOString().split('T')[0]} />
-        </div>
-      )
+
+  const handlePosition = (position: Position) => {
+    setSelectedPositions((prevPositions) => {
+    const index = prevPositions.indexOf(position);
+    if (index !== -1) {
+      // Si la posición ya está seleccionada, la eliminamos
+      return prevPositions.filter((_, i) => i !== index);
+    } else {
+      // Si la posición no está, la añadimos al final del array
+      return [...prevPositions, position];
     }
-  ]
+
+  });
+  }
 
   useEffect(() => {
     api.getTodayDailyPlayers().then((data) => {
       setDailyPlayers(data)
-      console.log(data)
     })
   }
   , [])
+
+  useEffect(() => {
+    
+  }, [selectedPositions])
 
   const handleDifficulty = (difficulty: Difficulty) => {
     setDifficultySelected(difficulty)
@@ -175,11 +84,101 @@ function App() {
   }
 
 
+
+   const characteristics: PlayerCharacteristic[] = [
+    {
+      name: 'Nationality',
+      apiName: 'nationality',
+      type: 'string',
+      placeholder: 'Select the player nationality',
+      component: 
+        <div className='flex justify-center gap-2'>
+          <select >
+            {orderedCountries.map((country, index) => (
+              <option key={index}>
+                {country.name}</option>
+            ))}
+          </select>
+        </div>
+      
+    },
+    {
+      name: 'Height',
+      apiName: 'heightCm',
+      type: 'number',
+      placeholder: 'Enter the height of the player',
+      component:
+        <div>
+        <input step="1" type='number' placeholder='Enter the height of the player' max={250} min={110} defaultValue={160} key='height'/>
+        </div>
+      
+    },
+    {
+      name: 'Weight',
+      apiName: 'weightKgs',
+      type: 'number',
+      placeholder: 'Enter the weight of the player',
+      component: 
+        <div className='flex justify-center gap-2'>
+        <input step="1" type='number' placeholder='Enter the weight of the player' max={200} min={40} defaultValue={70} key='weight'/>
+        <button
+              className={`bg-green-400 text-white p-2 font-bold rounded ${!characteristicSelected ? 'bg-gray-400 cursor-not-allowed' : 'hover:bg-green-500'}`}
+              disabled={!characteristicSelected}
+            >
+              Submit
+        </button>
+        </div>
+      
+    },
+    {
+      name: 'Preferred Foot',
+      apiName: 'preferredFoot',
+      type: 'string',
+      placeholder: 'Enter the preferred foot of the player',
+      component: 
+        <div className='flex justify-center gap-2'>
+          <button className='rounded bg-green-400 hover:bg-green-500 text-white font-bold py-2 px-6' value='Left'>Left</button>
+          <button className='rounded bg-green-400 hover:bg-green-500 text-white font-bold py-2 px-6' value='Right'>Right</button>
+        </div>
+      
+    },
+    {
+      name: 'Positions',
+      apiName: 'positions',
+      type: 'string',
+      placeholder: 'Enter the positions of the player',
+      component:
+        <div className='grid grid-cols-5 gap-2'>
+          {positions.map((position: Position, index: number): JSX.Element =>
+            <button
+              key={index}
+              className={`rounded bg-green-400 hover:bg-green-500 text-white font-bold py-2 px-6 ${selectedPositions.includes(position) ? 'bg-green-800' : ''}`}
+              onClick={() => handlePosition(position)}
+            >
+              {position}
+            </button>
+          )}      
+        </div>
+      
+    },
+    {
+      name: 'Birth Date',
+      apiName: 'birthDate',
+      type: 'string',
+      placeholder: 'Enter the birth date of the player',
+      component: 
+        <div className='flex justify-center gap-2' key={characteristicSelected?.name}>
+          <input  type='date' placeholder='Enter the birth date of the player' key='birthDate' max={new Date().toISOString().split('T')[0]} />
+        </div>
+      
+    }
+  ]
+
   const angle = 360 / characteristics.length
 
   return (
     
-    <div className='bg-green-200/60 min-h-screen '>
+    <div className='bg-green-200/60 min-h-screen mx-auto h-full'>
       {/* Header */}
   
       <section className={`container mx-auto p-8 text-center`}>
@@ -191,25 +190,25 @@ function App() {
       <section className={`container mx-auto text-center bg-green-300/40 p-8 rounded-sm`}>
         <div className="flex justify-center gap-2 gap-x-10">
           <button
-            className={`bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-6 rounded ${difficultySelected === Difficulty.EASY ? 'bg-green-800' : ''}`}
+            className={` hover:bg-green-700 text-white font-bold py-2 px-6 rounded ${difficultySelected === Difficulty.EASY ? 'bg-green-800' : 'bg-green-500'}`}
             onClick={() => handleDifficulty(Difficulty.EASY)}
           >
             Easy
           </button>
           <button
-            className={`bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-6 rounded ${difficultySelected === Difficulty.MEDIUM ? 'bg-green-800' : ''}`}
+            className={` hover:bg-green-700 text-white font-bold py-2 px-6 rounded ${difficultySelected === Difficulty.MEDIUM ? 'bg-green-800' : 'bg-green-500'}`}
             onClick={() => handleDifficulty(Difficulty.MEDIUM)}
           >
             Medium
           </button>
           <button
-            className={`bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-6 rounded ${difficultySelected === Difficulty.HARD ? 'bg-green-800' : ''}`}
+            className={` hover:bg-green-700 text-white font-bold py-2 px-6 rounded ${difficultySelected === Difficulty.HARD ? 'bg-green-800' : 'bg-green-500'}`}
             onClick={() => handleDifficulty(Difficulty.HARD)}
           >
             Hard
           </button>
           <button
-            className={`bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-6 rounded ${difficultySelected === Difficulty.VERY_HARD ? 'bg-green-800' : ''}`}
+            className={` hover:bg-green-700 text-white font-bold py-2 px-6 rounded ${difficultySelected === Difficulty.VERY_HARD ? 'bg-green-800' : 'bg-green-500'}`}
             onClick={() => handleDifficulty(Difficulty.VERY_HARD)}
           >
             Very Hard
@@ -218,7 +217,7 @@ function App() {
       </section>
 
       {/* Game sections */}
-      <div className="relative container mx-auto m-12">
+      <div className="relative container mx-auto mt-12">
         {/* Blur overlay */}
         {!difficultySelected && (
           <div className="absolute bg-green-200/10 backdrop-blur-sm z-30 p-24 w-full h-full"></div>
@@ -259,19 +258,15 @@ function App() {
           ) : (
             <h2 className="text-2xl font-bold">{characteristicSelected.placeholder}</h2>
           )}
-          <div className="flex justify-center gap-2 mt-4">
+          <div className="flex justify-center gap-2 mt-4" key={characteristicSelected?.name}>
             {characteristicSelected && (characteristicSelected.component ? (
-              characteristicSelected.component({
-                className: `bg-green-200/40 p-2 rounded ${border}`
-              })
-            ) : (
+              characteristicSelected.component
+            ): (
               <input
                 type={characteristicSelected?.type}
                 placeholder={characteristicSelected?.placeholder}
-                className={``}
               />
             ))}
-            
           </div>
         </section>
       </div>
---
+++ front/src/positions.tsx
@@ -0,0 +1,18 @@
+import { Position } from "./types/Positions.type";
+export const positions: Position[] = [
+    Position.CM,
+    Position.CB,
+    Position.ST,
+    Position.CDM,
+    Position.LM,
+    Position.RM,
+    Position.CAM,
+    Position.GK,
+    Position.RB,
+    Position.LB,
+    Position.LW,
+    Position.RW,
+    Position.CF,
+    Position.RWB,
+    Position.LWB,
+  ]
